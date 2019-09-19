0aa5a43ec33b3b4dad7ad02c01fc6d912a36ddbe
collin yablon <collin.yablon@travelershaven.com>
2019-09-19 14:35:07 -0600

    read values

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const gpio = require("rpi-gpio");
 const gpiop = gpio.promise;
 
+// gpiop
+//   .setup(16, gpio.DIR_OUT)
+//   .then(() => {
+//     return gpiop.write(16, false);
+//   })
+//   .catch(err => {
+//     console.log("Error: ", err.toString());
+//   });
+
 gpiop
-  .setup(16, gpio.DIR_OUT)
+  .setup(18, gpio.DIR_OUT)
   .then(() => {
-    return gpiop.write(16, false);
+    setInterval(() => {
+      gpiop.read(18, value => {
+        console.log(value);
+      });
+    }, 2000);
   })
   .catch(err => {
     console.log("Error: ", err.toString());
