1ad57aea6104271b2f61297f3e26ac8b4b108968
collin yablon <collin.yablon@travelershaven.com>
2019-09-19 13:33:14 -0600

    first index commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const gpio = require('rpi-gpio')
+const gpiop = gpio.promise;
+ 
+gpiop.setup(16, gpio.DIR_OUT)
+    .then(() => {
+        return gpiop.write(16, true)
+    })
+    .catch((err) => {
+        console.log('Error: ', err.toString())
+    })
