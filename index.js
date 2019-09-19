d81d8a75e15a606a530583087bda15f6960be7f4
collin yablon <collin.yablon@travelershaven.com>
2019-09-19 16:12:17 -0600

    updates

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const gpio = require("rpi-gpio");
 const express = require("express");
 const gpiop = gpio.promise;
+const PORT = 3000;
 
+const cache = {
+  ons: 0,
+  offs: 0,
+  lastOn: null,
+  lastOff: null
+};
 const app = express();
 
 const pin = gpiop.setup(16, gpio.DIR_OUT);
 
-app.get("/on", function(req, res) {
-  res.send("Hello World");
-  pin.then(() => gpiop.write(16, true));
-  res.send();
+app.get("/on", function(req, res, next) {
+  pin
+    .then(() => {
+      gpiop.write(16, true);
+      cache.ons += 1;
+      cache.lastOn = new Date().toString();
+      res.send();
+    })
+    .catch(next);
 });
 
-app.get("/off", function(req, res) {
-  res.send("Hello World");
-  pin.then(() => gpiop.write(16, false));
-  res.send();
+app.get("/off", function(req, res, next) {
+  pin
+    .then(() => {
+      gpiop.write(16, false);
+      cache.ons += 1;
+      cache.lastOff = new Date().toString();
+      res.send();
+    })
+    .catch(next);
 });
 
-app.listen(3000);
+app.get("/stats", (req, res, next) => {
+  res.send(cache);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
