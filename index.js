191dec7f840fc6c694bc934da644a040902d875c
collinsmith1054 <collin.smith@travelershaven.com>
2019-09-20 09:35:26 -0600

    fixing more syntax errors because im bad

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ app.get("/strobe", function(req, res, next) {
   pin
     .then(() => {
       var status = cache.status === "ON";
-      var i = 0;
-      for (i; i < 9; i++) {
+      for (var i = 0; i < 9; i++) {
         setTimeout(() => {
-          console.log('hit')
+          status = !status;
+          gpiop.write(16, status);
         }, 500)
       }
 
